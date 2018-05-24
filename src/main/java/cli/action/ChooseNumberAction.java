bf28249486ae49399cd53fa836f86eced192592c
Mike Moldawsky <mike.moldawsky@gmail.com>
2018-05-24 18:01:36 +0300

    Done - added Board View When Game completed

--- src/main/java/cli/action/ChooseNumberAction.java
+++ src/main/java/cli/action/ChooseNumberAction.java
@@ -8,19 +8,18 @@ public class ChooseNumberAction extends ActionView {
     private Integer numberChosen;
     private BoardGame puzzle;
 
-
     public ChooseNumberAction(BoardGame puzzle, Integer num) {
         super("You Chose To Move number: " + num + "\n", "Move number: " + num);
         this.puzzle = puzzle;
         this.numberChosen = num;
     }
 
-
     @Override
     public void executeCustomAction() {
         this.puzzle.moveNumberByValue(this.numberChosen);
         if(this.puzzle.isCompleted()){
-            System.out.println("#######################\nGOOD JOB - YOU MADE IT\n#######################\n");
+            this.puzzle.draw();
+            System.out.println("\n#######################\nGOOD JOB - YOU MADE IT\n#######################\n");
             Runtime.getRuntime().exit(0);
         } else{
             ChooseNumberMenu chooseNumberMenu = new ChooseNumberMenu(this.puzzle);
